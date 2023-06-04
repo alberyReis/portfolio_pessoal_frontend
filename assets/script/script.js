4e7fe847769ecc610c7972edd318efe57b79c046
Perkles <albery.reis.88@gmail.com>
2023-06-04 15:09:34 -0300

    update menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,15 @@
-const hamburguer = document.querySelector('.menu_hamburguer img')
-const body = document.querySelector('body')
-const navHamburguer = document.querySelector('.nav_hamburguer')
-const banner = document.querySelector('.banner')
-const li = document.querySelectorAll('.nav_hamburguer li')
-const [li1, li2, li3, li4, li5] = li
-
-hamburguer.addEventListener('click', () => {
-    if (navHamburguer.style.display == 'none') {
-        navHamburguer.style.display = 'flex'
-        banner.style.display = 'none' 
-    } else {
-        navHamburguer.style.display = 'none'
-        banner.style.display = 'block'
-    }
-})
-
-
-
-
-
-
-
-
+function menuHamburguer() {
+    const hamburguer = document.querySelector('.menu_hamburguer')
+    const navHamburguer = document.querySelector('.nav_hamburguer')
+    const banner = document.querySelector('.banner')
+    hamburguer.addEventListener('click', () => {
+        if (navHamburguer.style.display == 'none') {
+            navHamburguer.style.display = 'flex'
+            banner.style.display = 'none'
+        } else {
+            navHamburguer.style.display = 'none'
+            banner.style.display = 'block'
+        }
+    })
+}
+menuHamburguer()
