ee78f912765a179e344245f4ffff5071202f1cfd
Perkles <albery.reis.88@gmail.com>
2024-06-16 03:21:31 -0300

    adição do grid no projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 function menuHamburguer() {
     const hamburguer = document.querySelector('#hamburguer__icone')
     const navHamburguer = document.querySelector('#hamburguer__nav')
-    const banner = document.querySelector('#banner')
+    const banner = document.querySelector('#apresentacao')
     hamburguer.addEventListener('click', () => {
         if (navHamburguer.style.display == 'none') {
             navHamburguer.style.display = 'flex'
-            banner.style.display = 'none'
+            apresentacao.style.display = 'none'
         } else {
             navHamburguer.style.display = 'none'
-            banner.style.display = 'block'
+            apresentacao.style.display = 'block'
         }
     })
     navHamburguer.addEventListener('click', () => {
         if(navHamburguer.style.display == 'flex') {
             navHamburguer.style.display = 'none'
-            banner.style.display = 'block'
+            apresentacao.style.display = 'block'
         }
     })
 }
