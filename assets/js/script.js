de5d8066939aebe5f80b160019371e9a36d0bb16
Perkles <albery.reis.88@gmail.com>
2024-06-22 02:40:59 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,26 @@ const hamburguer = document.querySelector('#hamburguer__icone')
 const navHamburguer = document.querySelector('#hamburguer__nav')
 const banner = document.querySelector('#apresentacao')
 
-const menuHamburguerAberto = (navHamburguer, apresentacao) => {
+const abrirMenuHamburguer = (navHamburguer, apresentacao) => {
     navHamburguer.style.display = 'flex'
     apresentacao.style.display = 'none'
 }
 
-const menuHamburguerFechado = (navHamburguer, apresentacao) => {
+const fecharMenuHamburguer = (navHamburguer, apresentacao) => {
     navHamburguer.style.display = 'none'
     apresentacao.style.display = 'block'
 }
 
 hamburguer.addEventListener('click', () => {
     if (navHamburguer.style.display == 'none') {
-        menuHamburguerAberto(navHamburguer, apresentacao)
+        abrirMenuHamburguer(navHamburguer, apresentacao)
     } else {
-        menuHamburguerFechado(navHamburguer, apresentacao)
+        fecharMenuHamburguer(navHamburguer, apresentacao)
     }
 })
 
 navHamburguer.addEventListener('click', () => {
     if (navHamburguer.style.display == 'flex') {
-        menuHamburguerFechado(navHamburguer, apresentacao)
+        fecharMenuHamburguer(navHamburguer, apresentacao)
     }
 })
