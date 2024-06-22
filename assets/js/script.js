0d068804df3e7b0cbb5cf7616c14a855762d988a
Perkles <albery.reis.88@gmail.com>
2024-06-22 02:26:40 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,27 @@
-function menuHamburguer() {
-    const hamburguer = document.querySelector('#hamburguer__icone')
-    const navHamburguer = document.querySelector('#hamburguer__nav')
-    const banner = document.querySelector('#apresentacao')
-    hamburguer.addEventListener('click', () => {
-        if (navHamburguer.style.display == 'none') {
-            navHamburguer.style.display = 'flex'
-            apresentacao.style.display = 'none'
-        } else {
-            navHamburguer.style.display = 'none'
-            apresentacao.style.display = 'block'
-        }
-    })
-    navHamburguer.addEventListener('click', () => {
-        if (navHamburguer.style.display == 'flex') {
-            navHamburguer.style.display = 'none'
-            apresentacao.style.display = 'block'
-        }
-    })
+const hamburguer = document.querySelector('#hamburguer__icone')
+const navHamburguer = document.querySelector('#hamburguer__nav')
+const banner = document.querySelector('#apresentacao')
+
+const menuHamburguerAberto = (navHamburguer, apresentacao) => {
+    navHamburguer.style.display = 'flex'
+    apresentacao.style.display = 'none'
 }
-menuHamburguer()
+
+const menuHamburguerFechado = (navHamburguer, apresentacao) => {
+    navHamburguer.style.display = 'none'
+    apresentacao.style.display = 'block'
+}
+
+hamburguer.addEventListener('click', () => {
+    if (navHamburguer.style.display == 'none') {
+        menuHamburguerAberto(navHamburguer, apresentacao)
+    } else {
+        menuHamburguerFechado(navHamburguer, apresentacao)
+    }
+})
+
+navHamburguer.addEventListener('click', () => {
+    if (navHamburguer.style.display == 'flex') {
+        menuHamburguerFechado(navHamburguer, apresentacao)
+    }
+})
